7a14befb69b1fd5a1b19c7feea76ee8a7603a5f3
Md-Romaan <mohammadromaan2806@gmail.com>
2024-08-21 16:36:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <>
-    {/* <Login/> */}
-    <SignUp/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 const Login = () => {
 
     const inputDiv = "mb-4 flex flex-col"
-    const inputLabel = "mb-2 text-xl"
+    const inputLabel = "mb-2 text-sm"
     const inputCss = "p-2 border-2 rounded-md"
 
     const [formData, setFormData] = useState({
@@ -35,77 +35,74 @@ const Login = () => {
         setStatus("");
         console.log(formData);
 
-        
 
-          
-        
 
-        
+
+
+
+
 
     }
-
-
-
 
     return (
         <>
             <div className="container mx-auto flex justify-center items-center h-screen w-screen">
 
-                    <div className=" pt-5 pb-5 mt-5 mb-5 border-secondary  w-3/4 h-3/4 flex flex-col justify-center items-center" id="login-box">
-                        <h1 className="text-center p-2 text-5xl font-bold m-6">Login</h1>
+                <div className=" pt-5 pb-5 mt-5 mb-5 border-secondary  w-3/4 h-3/4 flex flex-col justify-center items-center" id="login-box">
+                    <h1 className="text-center p-2 text-5xl font-bold m-6">Login</h1>
 
-                        <form onSubmit={handleSubmit} className=" w-3/6 p-10">
-                            {error && <p className="">{error}</p>}
-                            {status && <p className="">{status}</p>}
+                    <form onSubmit={handleSubmit} className="rounded sm:w-[70%] md:w-3/6 p-10">
+                        {error && <p className="">{error}</p>}
+                        {status && <p className="">{status}</p>}
 
-                            <div className={inputDiv}>
-                                <label className={inputLabel} htmlFor="form1Example1">
-                                    Email
-                                </label>
-                                <input type="email" id="form1Example1" className={inputCss}
-                                    placeholder="Email" name="email"
-                                    onChange={handleChange}
-                                    value={formData.email} required />
-                            </div>
-                            <div className={inputDiv}>
-                                <label className={inputLabel} htmlFor="form1Example2">
-                                    Password
-                                </label>
-                                <input type="password" id="form1Example2" className={inputCss} placeholder="password"
-                                    name="password" onChange={handleChange}
-                                    value={formData.password} required />
-                            </div>
-                            <div className=" mb-4">
-                                <div className={inputLabel}>
-                                    <div className="">
-                                        <label className={inputLabel} htmlFor="form1Example3">
-                                            
-                                            Remember me{" "}
-                                        </label>
-                                        <input
-                                            className="bg-blue-600"
-                                            type="checkbox"
-                                            defaultValue=""
-                                            id="form1Example3"
-                                            defaultChecked=""
-                                        />
-                                    </div>
-                                </div>
-                                <div className={inputLabel}>
-                                    <a className="hover:text-blue-500" href="/verify">Forgot password?</a>
+                        <div className={inputDiv}>
+                            <label className={inputLabel} htmlFor="form1Example1">
+                                Email
+                            </label>
+                            <input type="email" id="form1Example1" className={inputCss}
+                                placeholder="Email" name="email"
+                                onChange={handleChange}
+                                value={formData.email} required />
+                        </div>
+                        <div className={inputDiv}>
+                            <label className={inputLabel} htmlFor="form1Example2">
+                                Password
+                            </label>
+                            <input type="password" id="form1Example2" className={inputCss} placeholder="password"
+                                name="password" onChange={handleChange}
+                                value={formData.password} required />
+                        </div>
+                        <div className=" mb-4">
+                            <div className={inputLabel}>
+                                <div className="">
+                                    <label className={inputLabel} htmlFor="form1Example3" >
+
+                                        Remember me{" "}
+                                    </label>
+                                    <input
+                                        className="bg-blue-600"
+                                        type="checkbox"
+                                        defaultValue=""
+                                        id="form1Example3"
+                                        defaultChecked=""
+                                    />
                                 </div>
                             </div>
-                            <div className="text-center">
-                                <button
-                                    type="submit"
-                                    className="bg-blue-400 text-white hover:text-blue-400 w-full py-2 rounded-md bottom-2 hover:bg-slate-200"
-                                >
-                                    Sign in
-                                </button>
+                            <div className={inputLabel}>
+                                <a className="hover:text-blue-500 text-sm" href="/verify">Forgot password?</a>
                             </div>
-                        </form>
+                        </div>
+                        <div className="text-center">
+                            <button
+                                type="submit"
+                                className="bg-blue-400 text-white hover:text-blue-400 w-full py-2 rounded-md bottom-2 hover:bg-slate-200"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
 
-                    </div>
+                </div>
 
 
             </div>
