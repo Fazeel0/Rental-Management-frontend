eeca9673078e6bbe65e082721ab76527cd107212
Mohammed Fazeel <fazeelmuchale@gmail.com>
2024-08-21 17:02:35 +0530

    added Customer Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import Branch from './components/Create Forms/Branch';
+import Customer from './components/Create Forms/Customer';
 import RentalForm from './components/Create Forms/Rental';
 import BranchUpdate from './components/Update Forms/BranchUpdate';
+import CustomerUpdate from './components/Update Forms/CustomerUpdate';
 import RentalUpdate from './components/Update Forms/RentalUpdate';
 import Login from './pages/Login';
+import Product from './pages/ProductDetails';
 import SignUp from './pages/SignUp';
 
 function App() {
@@ -15,7 +18,8 @@ function App() {
     {/* <RentalForm/> */}
     {/* <Branch/> */}
     {/* <BranchUpdate/> */}
-    <RentalUpdate/>
+    {/* <RentalUpdate/> */}
+    <Product/>
     </>
   );
 }
---
+++ src/components/Create Forms/Customer.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { useState } from "react"
+// import axios from "axios";
+
+
+const Customer = () => {
+
+    const inputDiv = "mb-4 flex flex-col"
+    const inputLabel = "mb-2 text-xl"
+    const inputCss = "p-2 border-2 rounded-md"
+
+    const [formData, setFormData] = useState({
+        name: "",
+        phoneNo : "",
+        address: "",
+    })
+
+    const [error, setError] = useState("");
+    const [status, setStatus] = useState("");
+
+
+    //whenever user types in input fields, handles ==>
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setStatus("");
+        console.log(formData);
+
+        
+
+          
+        
+
+        
+
+    }
+
+
+
+
+    return (
+        <>
+            <div className="container mx-auto flex justify-center items-center h-screen w-screen">
+
+                    <div className=" pt-5 pb-5 mt-5 mb-5 border-secondary  w-3/4 h-3/4 flex flex-col justify-center items-center" id="login-box">
+                        <h1 className="text-center p-2 text-5xl font-bold m-6">Create Customer</h1>
+
+                        <form onSubmit={handleSubmit} className=" w-3/6 p-10">
+                            {error && <p className="">{error}</p>}
+                            {status && <p className="">{status}</p>}
+
+                            <div className={inputDiv}>
+                                <label className={inputLabel} htmlFor="form1Example1">
+                                    Customer Name
+                                </label>
+                                <input type="text" id="form1Example1" className={inputCss}
+                                    placeholder="Customer Name" name="name"
+                                    onChange={handleChange}
+                                    value={formData.name} required />
+                            </div>
+                            <div className={inputDiv}>
+                                <label className={inputLabel} htmlFor="form1Example2">
+                                    Phone Number
+                                </label>
+                                <input type="Number" id="form1Example2" className={inputCss} placeholder="Phone Number"
+                                    name="phoneNo" onChange={handleChange}
+                                    value={formData.phoneNo} required />
+                            </div>
+                            <div className={inputDiv}>
+                                <label className={inputLabel} htmlFor="form1Example2">
+                                    Address
+                                </label>
+                                <input type="text" id="form1Example2" className={inputCss} placeholder="Address"
+                                    name="address" onChange={handleChange}
+                                    value={formData.address} required />
+                            </div>
+                                
+                            <div className="text-center">
+                                <button
+                                    type="submit"
+                                    className="bg-blue-400 text-white hover:text-blue-400 w-full py-2 rounded-md bottom-2 hover:bg-slate-200"
+                                >
+                                    Create Customer
+                                </button>
+                            </div>
+                        </form>
+
+                    </div>
+
+
+            </div>
+
+
+        </>
+    )
+}
+
+export default Customer;
