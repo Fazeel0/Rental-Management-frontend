4c418f531767f4493a490697b2072f690a741f01
Md-Romaan <mohammadromaan2806@gmail.com>
2024-08-28 11:03:38 +0530

    updates

--- src/components/DisplayDetails/AllRents.jsx
+++ src/components/DisplayDetails/AllRents.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 const AllRents = () => {
 
-    const [rental, setrental] = useState();
+    const [rentals, setrentals] = useState();
 
     useEffect(() => {
 
@@ -12,7 +12,7 @@ const AllRents = () => {
             try {
                 const response = await axios.get("/rental/all");
                 if (response.data.success) {
-                    setrental(response.data.rentalProducts);
+                    setrentals(response.data.rentalProducts);
                 }
             } catch (error) {
                 console.log(error);
@@ -23,11 +23,45 @@ const AllRents = () => {
 
     }, []);
 
-    console.log(rental);
+    console.log(rentals);
 
     return (
         <>
-        
+            <div className="overflow-x-auto mt-5">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th className='text-base text-black'>Customer name</th>
+                            <th className='text-base text-black'>Product name</th>
+                            <th className='text-base text-black'>Balance Amount</th>
+                            <th className='text-base text-black'>Paid Amount</th>
+                            <th className='text-base text-black'>Quantity</th>
+                            <th className='text-base text-black'>More details</th>
+                        </tr>
+                    </thead>
+                    {rentals?.map((rent, index) => {
+                        return (
+                            <>
+                                <tbody>
+                                    {/* row 1 */}
+                                    <tr>
+                                        <td>{index + 1}</td>
+                                        <td>{rent?.customer?.name}</td>
+                                        <td>{rent?.product?.name}</td>
+                                        <td>{rent?.balanceAmount}</td>
+                                        <td>{rent?.paidAmount}</td>
+                                        <td>{rent?.quantity}</td>
+                                        <td><button className='btn btn-primary text-white'>More details</button></td>
+                                    </tr>
+                                </tbody>
+                            </>
+                        )
+                    })}
+
+                </table>
+            </div>
         </>
     )
 }
