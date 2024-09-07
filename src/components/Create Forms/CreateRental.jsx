e49e37e9aaa3fc821a7ad938c7a0e2ef4a0aea27
Md-Romaan <mohammadromaan2806@gmail.com>
2024-09-07 16:27:23 +0530

    UI changed for create product

--- src/components/Create Forms/CreateRental.jsx
+++ src/components/Create Forms/CreateRental.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useRef } from "react";
 import { useState } from "react"
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-// import axios from "axios";
 
 
 const CreateRental = () => {
@@ -65,8 +64,6 @@ const CreateRental = () => {
             hasMounted.current = true;
         }
 
-
-
     }, [selectedBranch]);
 
 
@@ -90,7 +87,14 @@ const CreateRental = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         let formData = new FormData(e.target);
-        const data = Object.fromEntries(formData.entries());
+        let data = Object.fromEntries(formData.entries());
+
+        let totalQuantity = 0;
+        rows.forEach((row) => {
+            totalQuantity = totalQuantity + Number(row.quantity);
+        })
+
+        data = { ...data, products: rows, totalQuantity };
 
         try {
             const response = await axios.post("/rental/add", data)
@@ -104,94 +108,145 @@ const CreateRental = () => {
         }
     }
 
+
+    //Table 
+    const [rows, setRows] = useState([{ id: '', quantity: '' }]);
+
+    const handleAddRow = () => {
+        setRows([...rows, { id: '', quantity: '' }]);
+    };
+
+    const handleInputChange = (index, field, value) => {
+        const updatedRows = [...rows];
+        updatedRows[index][field] = value;
+        setRows(updatedRows);
+    };
+
     return (
         <>
             <div>
                 <h1 className="text-2xl font-bold text-center mt-6 bg-blue-300 rounded-lg mx-4">Rent a Product</h1>
-                <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-5 mx-10 mt-6">
-                    <div>
-                        <label htmlFor="startDate" className="font-bold">Rent Date:</label>
-                        <input id="startDate"
-                            type="date"
-                            name="startDate"
-                            placeholder="Rent date"
-                            className="input input-bordered input-info w-full" required />
-                    </div>
-                    <div>
-                        <label htmlFor="startDate" className="font-bold">Return Date:</label>
-                        <input id="startDate"
-                            type="date"
-                            name="endDate"
-                            placeholder="Return date"
-                            className="input input-bordered input-info w-full" required />
-                    </div>
-                    <div>
-                        <label htmlFor="paidAmount" className="font-bold">Paid Amount:</label>
-                        <input id="paidAmount"
-                            type="text"
-                            name="paidAmount"
-                            placeholder="Enter amount paid"
-                            className="input input-bordered input-info w-full" required />
-                    </div>
+                <form onSubmit={handleSubmit} >
+                    <div className="grid grid-cols-2 gap-5 mx-10 mt-6">
+                        <div>
+                            <label htmlFor="startDate" className="font-bold">Rent Date:</label>
+                            <input id="startDate"
+                                type="date"
+                                name="startDate"
+                                placeholder="Rent date"
+                                className="input input-bordered input-info w-full" required />
+                        </div>
+                        <div>
+                            <label htmlFor="startDate" className="font-bold">Return Date:</label>
+                            <input id="startDate"
+                                type="date"
+                                name="endDate"
+                                placeholder="Return date"
+                                className="input input-bordered input-info w-full" required />
+                        </div>
+                        <div>
+                            <label htmlFor="paidAmount" className="font-bold">Paid Amount:</label>
+                            <input id="paidAmount"
+                                type="text"
+                                name="paidAmount"
+                                placeholder="Enter amount paid"
+                                className="input input-bordered input-info w-full" required />
+                        </div>
 
 
-                    <div>
-                        <label htmlFor="customer" className="font-bold">Customer:</label>
-                        <input id="customer"
-                            type="text"
-                            name="customer"
-                            value={id}
-                            className="input input-bordered input-info w-full" readOnly />
+                        <div>
+                            <label htmlFor="customer" className="font-bold">Customer:</label>
+                            <input id="customer"
+                                type="text"
+                                name="customer"
+                                value={id}
+                                className="input input-bordered input-info w-full" readOnly />
+                        </div>
+
+                        <div>
+                            <label htmlFor="branch" className="font-bold">Branch:</label>
+                            <select onChange={changeBranch} name="branch" id="branch" className="select select-info w-full" required>
+                                <option disabled selected>Select Branch</option>
+                                {branches?.map((branch) => {
+                                    return (
+                                        <>
+                                            <option value={branch?._id}>{branch?.name}</option>
+                                        </>
+                                    )
+                                })}
+                            </select>
+                        </div>
                     </div>
 
-                    <div>
-                        <label htmlFor="branch" className="font-bold">Branch:</label>
-                        <select onChange={changeBranch} name="branch" id="branch" className="select select-info w-full" required>
-                            <option disabled selected>Select Branch</option>
-                            {branches?.map((branch) => {
-                                return (
-                                    <>
-                                        <option value={branch?._id}>{branch?.name}</option>
-                                    </>
-                                )
-                            })}
-                        </select>
-                    </div>
 
-                    <div>
-                        <label htmlFor="product" className="font-bold">Product:</label>
-                        <select onChange={changeProduct} name="product" className="select select-info w-full" disabled={disableProduct} required>
-                            <option disabled selected>Select Product</option>
-                            {products?.map((product) => {
-                                return (
-                                    <>
-                                        <option value={product?._id}>{product?.name}</option>
-                                    </>
-                                )
-                            })}
+                    {/* Table................................................................. */}
+                    <table className="w-[70%] bg-white border mx-10 my-4">
+                        <thead>
+                            <tr>
+                                <th className="px-4 py-2 border">Product</th>
+                                <th className="px-4 py-2 border">Quantity</th>
+                                <th className="px-4 py-2 border"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows?.map((row, index) => (
+                                <tr key={index}>
+                                    <td className="px-4 py-2 border">
+                                        <input
+                                            type="text"
+                                            list="suggestion"
+                                            className="w-full px-2 py-1 border rounded"
+                                            placeholder="Enter product"
+                                            value={row?.id}
+                                            onChange={(e) => handleInputChange(index, 'id', e.target.value)}
+                                        />
 
-                        </select>
-                    </div>
+                                        <datalist id="suggestion" >
+                                            {products?.map((product) => {
+                                                return <>
+                                                    <option value={product?._id}>{product?.name}</option>
+                                                </>
+                                            })}
+                                        </datalist>
 
-                    <div>
-                        <label htmlFor="quantity" className="font-bold">Quantity:</label>
-                        {availableQty ? <span className="text-green-700 font-bold"> In Stock {availableQty}</span> : null}
-                        <input id="quantity"
-                            type="number"
-                            name="quantity"
-                            max={availableQty}
-                            placeholder="Enter Rented quantity"
-                            className="input input-bordered input-info w-full" disabled={disableQty} required />
-                    </div>
+                                    </td>
+                                    <td className="px-4 py-2 border">
+                                        <input
+                                            type="number"
+                                            className="w-full px-2 py-1 border rounded"
+                                            placeholder="Enter quantity"
+                                            value={row.quantity}
+                                            onChange={(e) => handleInputChange(index, 'quantity', e.target.value)}
+                                        />
+                                    </td>
+                                    <td className="px-4 py-2 border text-center">
+                                        {index === rows.length - 1 && (
+                                            <button
+                                                className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded-full"
+                                                onClick={handleAddRow}>
+                                                +
+                                            </button>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
 
-                    <div>
+
+
+                    <div className="mx-10">
                         <button type="submit" className="btn btn-success text-white font-bold mr-2">Submit</button>
                         <button onClick={() => window.location.reload()} type="button" className="btn btn-error text-white font-bold">Reset</button>
                     </div>
 
-
-
                 </form>
+
+
+
+
+
+
             </div>
         </>
     )
--- src/components/Create Forms/Createproduct.jsx
+++ src/components/Create Forms/Createproduct.jsx
@@ -78,8 +78,7 @@ const CreateProduct = () => {
       <div className="container mx-auto flex justify-center items-center h-screen">
         <div
           className=" pt-5 pb-5 mt-5 mb-5 border-secondary  w-2/4 h-3/4 flex flex-col justify-center items-center"
-          id="login-box"
-        >
+          id="login-box">
           <h1 className="text-center p-2 text-4xl font-bold m-6">
             Add Product
           </h1>
